43427e46eb4109c6f034682511eccd5653c148d9
Forrik <ilya33533@mail.ru>
2024-01-03 22:36:10 +0500

    registration functionality added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+import {
+    validationResult
+} from 'express-validator';
+import {
+    registerValidation
+} from './validations/auth.js';
+import userModel from './models/User.js';
 
-mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.vksn84v.mongodb.net/?retryWrites=true&w=majority')
+
+mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.vksn84v.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('Подключение к базе данных успешно выполнено!'))
     .catch((err) => console.log('Произошла ошибка', err));
 
@@ -11,27 +20,53 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World! Hello World!');
-})
 
-app.post('/auth/login', (req, res) => {
-    console.log(req.body);
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Некорректные данные при регистрации'
 
-    const token = jwt.sign({
-        email: req.body.email,
-        password: req.body.password,
-        fullName: req.body.fullName,
-    }, 'secret', {
-        expiresIn: '1h'
-    });
+            })
+        }
 
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
 
+        const doc = new userModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        })
 
-    res.json({
-        success: true,
-        token
-    })
+        const user = await doc.save();
+
+        const token = jwt.sign({
+
+            _id: user._id,
+        }, 'secret123', {
+            expiresIn: '30d'
+        }, )
+
+        const {
+            passwordHash,
+            ...userData
+        } = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось зарегистрироваться'
+        })
+    }
 })
 
 app.listen(4444, (err) => {
