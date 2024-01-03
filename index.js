a0e56083604dacc08c6925960f8601db030f1fb4
Forrik <ilya33533@mail.ru>
2024-01-03 22:58:53 +0500

    login functionality added

--- index.js
+++ index.js
@@ -20,6 +20,54 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/auth/login', async (req, res) => {
+    try {
+
+        const user = await userModel.findOne({
+            email: req.body.email
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден'
+            })
+        }
+
+        const isValidPassword = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if (!isValidPassword) {
+            return res.status(404).json({
+                message: 'Неверный логин или пароль'
+            })
+        }
+
+        const token = jwt.sign({
+
+                _id: user._id,
+            },
+            'secret123', {
+                expiresIn: '30d'
+            }
+        )
+
+        const {
+            passwordHash,
+            ...userData
+        } = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось авторизоваться'
+        })
+
+    }
+})
 
 app.post('/auth/register', registerValidation, async (req, res) => {
     try {
